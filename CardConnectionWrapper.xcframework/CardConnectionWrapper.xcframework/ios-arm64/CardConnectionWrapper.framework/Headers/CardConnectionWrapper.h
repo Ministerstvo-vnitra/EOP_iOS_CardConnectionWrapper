643b72152365288051e845f41914c7f76c6e3162
Testing <info@ahead-itec.com>
2024-09-23 20:38:56 +0200

    CardConnectionWrapper v2.0.1

---
+++ CardConnectionWrapper.xcframework/CardConnectionWrapper.xcframework/ios-arm64/CardConnectionWrapper.framework/Headers/CardConnectionWrapper.h
@@ -0,0 +1,18 @@
+//
+//  CardConnectionWrapper.h
+//  CardConnectionWrapper
+//
+//  Created by Majo on 22/05/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CardConnectionWrapper.
+FOUNDATION_EXPORT double CardConnectionWrapperVersionNumber;
+
+//! Project version string for CardConnectionWrapper.
+FOUNDATION_EXPORT const unsigned char CardConnectionWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CardConnectionWrapper/PublicHeader.h>
+
+
