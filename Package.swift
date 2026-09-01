51b7f80f88af301a193306d2c32c8d3e3cc3567e
ahead-ci <info@ahead-itec.com>
2026-09-01 10:35:08 +0200

    CardConnectionWrapper v2.0.5

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "CardConnectionWrapper",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v15),
     ],
     products: [
         .library(
